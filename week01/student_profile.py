dbc4f6f8f191a8343f03bb6635af4e9f5f861c23
Gülce Gedikli <gulcegedikli41@gmail.com>
2026-09-16 15:18:21 +0300

    Create student_profile.py

---
+++ week01/student_profile.py
@@ -0,0 +1,8 @@
+# Kullanıcıdan istenen 4 bilgiyi alıyoruz
+name = input("Enter your name: ")
+department = input("Enter your department: ")
+age = input("Enter your age: ")
+career_goal = input("Enter your career goal: ")
+
+# Bilgileri istenilen formatta ekrana yazdırıyoruz
+print(f"--- Student Profile --- Name: {name} Department: {department} Age: {age} Career Goal: {career_goal}")

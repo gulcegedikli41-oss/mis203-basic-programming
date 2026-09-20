2ee26a2a66d2673db9ef76cc1ca4e78862e4d726
Gülce Gedikli <gulcegedikli41@gmail.com>
2026-09-20 20:52:50 +0300

    Update student_profile.py

--- week01/student_profile.py
+++ week01/student_profile.py
@@ -1,8 +1,7 @@
-# Kullanıcıdan istenen 4 bilgiyi alıyoruz
 name = input("Enter your name: ")
 department = input("Enter your department: ")
 age = int(input("Enter your age: "))
 career_goal = input("Enter your career goal: ")
-
-# Bilgileri istenilen formatta ekrana yazdırıyoruz
-print(f"--- Student Profile --- Name: {name} Department: {department} Age: {age} Career Goal: {career_goal}")
+all_information =[f"---Student Profile---", f"Name: {name}", f"Department: {department}", f"Age: {age}", f"Career Goal: {career_goal}"]
+for information in all_information:
+  print(information)

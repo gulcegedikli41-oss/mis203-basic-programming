d1dd3af902b00c1298b5e1d04e7e7cc80d1ab4f3
Gülce Gedikli <gulcegedikli41@gmail.com>
2026-09-26 00:09:10 +0300

    Update grade_calculator.py

--- week02/grade_calculator.py
+++ week02/grade_calculator.py
@@ -5,7 +5,7 @@ while True:
     if name == "q":
         break
     else:
-        score = float(input("Enter score:"))
+        score = int(input("Enter score:"))
         if score < 0 or score > 100:
             print("Invalid score. Please enter a number between 0 and 100.")
             continue
